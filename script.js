2b3015101371cc98abef6aa8a6ef57ca377142ce
ktx203 <78192053+ktx203@users.noreply.github.com>
2021-11-08 07:55:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+const quoteWall = document.getElementById('quoteWall')
+// var count = document.getElementById('count');
+// var input = document.getElementById('input');
+var counter = 1;
+
+quoteInputElement.addEventListener('input', () => {
+ const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+ const arrayInput = quoteInputElement.value.split('')
+
+ let allCorrect = true
+ arrayQuote.forEach((characterSpan, index) => {
+ 	const characterInput = arrayInput[index]
+ 	if (characterInput == null) {
+ 		characterSpan.classList.remove('correct')
+ 		characterSpan.classList.remove('incorrect')
+ 		allCorrect = false 
+ 	}
+ 	else if (characterInput === characterSpan.innerText) {
+ 		characterSpan.classList.add('correct')
+ 		characterSpan.classList.remove('incorrect')
+ 	} 
+ 	else {
+ 		characterSpan.classList.remove('correct')
+ 		characterSpan.classList.add('incorrect')
+ 		allCorrect = false
+ 	}
+ })
+
+	var displayquote = arrayInput.join('')
+	console.log(displayquote)
+if (allCorrect) {
+	const quotewallpaper = document.createElement("P")
+	quotewallpaper.textContent = displayquote
+	quoteWall.appendChild(quotewallpaper)
+	quotewallpaper.classList.add('background-text')
+}
+if (allCorrect && counter < 5) renderNewQuote()
+
+
+})
+
+
+function getRandomQuote() {
+	return fetch(RANDOM_QUOTE_API_URL)
+		.then(response => response.json())
+		.then(data => data.content)
+
+}
+
+
+async function renderNewQuote() {
+	const quote = await getRandomQuote()
+	//const quote = "test"
+	quoteDisplayElement.innerHTML = ''
+	quote.split('').forEach(character => {
+		const characterSpan = document.createElement('span')
+		characterSpan.innerText = character
+	 	quoteDisplayElement.appendChild(characterSpan)
+	})
+	quoteInputElement.value = null
+	counter++
+	startTimer()
+}
+
+let startTime
+
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
+
+function hide() {
+  var x = document.getElementById("box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (timerElement.style.display === "none") {
+    timerElement.style.display = "block";
+  } else {
+    timerElement.style.display = "none";
+  }
+}
+
+// var timeLeft = 30;
+// var elem = document.getElementById('timer');
+
+   
+// timerId = setInterval(countdown, 1000);
+    
+// // function startTimer() {
+// //   timerElement.innerText = 0
+// //   startTime = new Date()
+// //   setInterval(() => {
+// //     timer.innerText = getTimerTime()
+// //   }, 1000)
+
+// function countdown() {
+//       if (timeLeft == -1) {
+//         clearTimeout(timerId);
+//         doSomething();
+//       } else {
+//         elem.innerHTML = timeLeft;
+//         timeLeft--;
+//       }
+//     }
+// countdown()
+// function wordCounter(text) {
+//   var text = quoteInput.value;
+//   var wordCount = 0;
+//   for (var i = 0; i <= text.length; i++) {
+//     if (text.charAt(i) == ' ') {
+//       wordCount++;
+//     }
+//   }
+//   count.innerText = wordCount;
+// }
+
+// // function doSomething(){
+// // 	quoteDisplayElement = wordCount
+// // }
+
+// renderNewQuote()
